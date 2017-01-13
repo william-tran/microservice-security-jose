4df34d25e7a4071f9d35051f61c25cb4ccca01a7
Will Tran <wtran@pivotal.io>
2017-01-13 11:28:54 -0500

    Outbound Message

---
+++ src/main/java/io/pivotal/spring/cloud/security/InvalidMessageException.java
@@ -0,0 +1,14 @@
+package io.pivotal.spring.cloud.security;
+
+public class InvalidMessageException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1230373186457021666L;
+
+	public InvalidMessageException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/io/pivotal/spring/cloud/security/Message.java
@@ -0,0 +1,38 @@
+package io.pivotal.spring.cloud.security;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Singular;
+
+@Builder
+@Getter
+public class Message {
+	private String initialToken;
+	private String parentToken;
+	@Singular("audience")
+	private List<String> audience;
+	private String request;
+	private Integer ttlSeconds;
+	@Singular("claim")
+	private Map<String,Object> customClaims;
+	private String contentType;
+	private byte[] body;
+	
+	public void validate() throws InvalidMessageException {
+		if (initialToken != null && parentToken != null) {
+			throw new InvalidMessageException("Both initialToken and parentToken cannot be set.");
+		}
+		if (audience == null || audience.isEmpty()) {
+			throw new InvalidMessageException("Audience must have at least one value.");
+		}
+		if (request == null) {
+			throw new InvalidMessageException("Request must be set.");
+		}
+		if (body == null && contentType != null || body != null && contentType == null ) {
+			throw new InvalidMessageException("Both body and contentType must be null or not null");
+		}
+	}
+}
